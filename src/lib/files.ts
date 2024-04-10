58ddb3d86ff566f400575a5a5503cbf2c3b0287a
xypnox <xypnox@gmail.com>
2024-04-10 15:49:33 +0530

    Add chapters and nav

--- src/lib/files.ts
+++ src/lib/files.ts
@@ -6,3 +6,11 @@ export async function readFileAsString(filePath: string) {
   const content = fs.readFileSync(fullPath, 'utf-8');
   return content;
 }
+
+export async function getFolders(dirPath: string) {
+  const fullPath = path.resolve(process.cwd(), dirPath);
+  console.log(fullPath);
+  const files = fs.readdirSync(fullPath, { withFileTypes: true });
+  return files.filter((file) => file.isDirectory()).map((file) => file.name);
+  // return []
+}
