8d0be85db479487b23574c86b8a23d3c634f8cf4
xypnox <xypnox@gmail.com>
2024-04-10 19:59:42 +0530

    Add better syntax highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import shikiTheme from "./shiki.json";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: shikiTheme,
+    }
+  },
+});
