a22b1cee7c2ac46873afd1b9a40e6acc44b9bf4d
xypnox <xypnox@gmail.com>
2024-04-10 14:45:03 +0530

    Improve structure

--- src/lib/files.ts
+++ src/lib/files.ts
@@ -1,25 +1,6 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
-export interface BaseItem {
-  path: string;
-  name: string;
-}
-
-export interface File extends BaseItem {
-  type: 'file';
-  content: string;
-  Component?: any
-}
-
-export type Item = File | Directory;
-
-export interface Directory extends BaseItem {
-  type: 'directory';
-  children: Item[];
-}
-
-
 export async function readFileAsString(filePath: string) {
   const fullPath = path.resolve(process.cwd(), filePath);
   const content = fs.readFileSync(fullPath, 'utf-8');
