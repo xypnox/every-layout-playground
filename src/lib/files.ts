cb0168b8714c1092f7df0f9ac813c8ff2140b3b3
xypnox <xypnox@gmail.com>
2024-05-16 21:14:01 +0530

    Make The Examples Render In Iframes
    Why: Iframe isolates and creates a new dom allowing, global styles can
    be applied without worrying about special keywords or classes
    
    We cheat a little by adding the default styles to the elements as well.
    So they are not as pure as they "should" be.

--- src/lib/files.ts
+++ src/lib/files.ts
@@ -12,5 +12,23 @@ export async function getFolders(dirPath: string) {
   console.log(fullPath);
   const files = fs.readdirSync(fullPath, { withFileTypes: true });
   return files.filter((file) => file.isDirectory()).map((file) => file.name);
-  // return []
+}
+
+export async function getFiles(dirPath: string): Promise<string[]> {
+  const result: string[] = [];
+  const fullPath = path.resolve(process.cwd(), dirPath);
+  const files = fs.readdirSync(fullPath, { withFileTypes: true });
+
+  for (const file of files) {
+    if (file.isDirectory()) {
+      // If the file is a directory, we want to go recursive
+      const subFiles = await getFiles(path.join(dirPath, file.name));
+      const newFiles = subFiles.map((subFile) => path.join(file.name, subFile));
+      result.push(...newFiles);
+    } else {
+      // the file is a file, we want to add the file name
+      result.push(file.name);
+    }
+  }
+  return result;
 }
